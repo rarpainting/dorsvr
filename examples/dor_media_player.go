30bed32ea97c0956b191e08e6782dd4e731450c7
Rarpainting <zhandequan@yahoo.com>
2019-07-04 21:29:36 +0800

    update

--- examples/dor_media_player.go
+++ examples/dor_media_player.go
@@ -2,25 +2,21 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
 
 	"github.com/djwackey/dorsvr/rtspclient"
 )
 
+var (
+	rtspURL = flag.String("rtsp-url", "", "")
+)
+
 func main() {
 	flag.Parse()
-	if flag.NArg() < 1 {
-		fmt.Println("Please input rtsp url.")
-		return
-	}
-
-	rtsp_url := os.Args[1]
 
 	client := rtspclient.New()
 
 	// to connect rtsp server
-	if !client.DialRTSP(rtsp_url) {
+	if !client.DialRTSP(*rtspURL) {
 		return
 	}
 
